0d6ea69d2113d339263e30f179879d8cc8c6aa15
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2015-05-12 11:27:14 -0400

    added sound

--- COMP397-Lesson02-Part2/Scripts/game.js
+++ COMP397-Lesson02-Part2/Scripts/game.js
@@ -5,8 +5,19 @@
 // Game Framework Variables
 var canvas = document.getElementById("canvas");
 var stage;
+var assets;
 // Game Variables
 var helloLabel; // create a reference
+var pinkButton;
+function preload() {
+    assets = new createjs.LoadQueue();
+    assets.installPlugin(createjs.Sound);
+    assets.on("complete", init, this);
+    assets.loadManifest([
+        { id: "pinkButton", src: "assets/images/pinkButton.png" },
+        { id: "clicked", src: "assets/audio/clicked.wav" }
+    ]);
+}
 function init() {
     stage = new createjs.Stage(canvas);
     stage.enableMouseOver(20);
@@ -18,6 +29,15 @@ function init() {
 function gameLoop() {
     stage.update();
 }
+function pinkButtonClicked(event) {
+    createjs.Sound.play("clicked");
+}
+function pinkButtonOver() {
+    pinkButton.alpha = 0.8;
+}
+function pinkButtonOut() {
+    pinkButton.alpha = 1.0;
+}
 // Our Main Game Function
 function main() {
     console.log("Game is Running");
@@ -25,7 +45,16 @@ function main() {
     helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
     helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     helloLabel.x = 160;
-    helloLabel.y = 220;
+    helloLabel.y = 190;
     stage.addChild(helloLabel);
+    pinkButton = new createjs.Bitmap(assets.getResult("pinkButton"));
+    pinkButton.regX = pinkButton.getBounds().width * 0.5;
+    pinkButton.regY = pinkButton.getBounds().height * 0.5;
+    pinkButton.x = 160;
+    pinkButton.y = 270;
+    stage.addChild(pinkButton);
+    pinkButton.on("click", pinkButtonClicked);
+    pinkButton.on("mouseover", pinkButtonOver);
+    pinkButton.on("mouseout", pinkButtonOut);
 }
 //# sourceMappingURL=game.js.map
--- COMP397-Lesson02-Part2/Scripts/game.ts
+++ COMP397-Lesson02-Part2/Scripts/game.ts
@@ -7,11 +7,23 @@
 // Game Framework Variables
 var canvas = document.getElementById("canvas");
 var stage: createjs.Stage;
+var assets: createjs.LoadQueue;
 
 
 // Game Variables
 var helloLabel: createjs.Text; // create a reference
+var pinkButton: createjs.Bitmap;
 
+
+function preload() {
+    assets = new createjs.LoadQueue();
+    assets.installPlugin(createjs.Sound);
+    assets.on("complete", init, this);
+    assets.loadManifest([
+        { id: "pinkButton", src: "assets/images/pinkButton.png" },
+        { id: "clicked", src: "assets/audio/clicked.wav" }
+    ]);
+}
 
 function init() {
     stage = new createjs.Stage(canvas);
@@ -27,6 +39,18 @@ function gameLoop() {
     stage.update();
 }
 
+function pinkButtonClicked(event: createjs.MouseEvent) {
+    createjs.Sound.play("clicked");
+}
+
+function pinkButtonOver() {
+    pinkButton.alpha = 0.8;
+}
+
+function pinkButtonOut() {
+    pinkButton.alpha = 1.0;
+}
+
 // Our Main Game Function
 function main() {
     console.log("Game is Running");
@@ -34,8 +58,16 @@ function main() {
     helloLabel.regX = helloLabel.getMeasuredWidth() * 0.5;
     helloLabel.regY = helloLabel.getMeasuredHeight() * 0.5;
     helloLabel.x = 160;
-    helloLabel.y = 220;
-
-
+    helloLabel.y = 190;
     stage.addChild(helloLabel);
+
+    pinkButton = new createjs.Bitmap(assets.getResult("pinkButton"));
+    pinkButton.regX = pinkButton.getBounds().width * 0.5;
+    pinkButton.regY = pinkButton.getBounds().height * 0.5;
+    pinkButton.x = 160;
+    pinkButton.y = 270;
+    stage.addChild(pinkButton);
+    pinkButton.on("click", pinkButtonClicked);
+    pinkButton.on("mouseover", pinkButtonOver);
+    pinkButton.on("mouseout", pinkButtonOut);
 }
